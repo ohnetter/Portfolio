c3955b0251572a2b2339ebaf87ab4e5c9edb93f5
ohnetter <ohnetter@gmail.com>
2023-01-02 16:26:45 -0500

    changed css

--- assets/script.js
+++ assets/script.js
@@ -23,14 +23,3 @@ function switchTheme(e) {
 }
 
 
-const currentTheme = localStorage.getItem("theme")
-  ? localStorage.getItem("theme")
-  : null;
-
-if (currentTheme) {
-  document.documentElement.setAttribute("data-theme", currentTheme);
-
-  if (currentTheme === "dark") {
-    toggleSwitch.checked = true;
-  }
-}
---
+++ assets/toggle.js
@@ -0,0 +1,11 @@
+const currentTheme = localStorage.getItem("theme")
+  ? localStorage.getItem("theme")
+  : null;
+
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+  }
+}
